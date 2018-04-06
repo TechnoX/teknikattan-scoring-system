f5ecd25d26b012582af0c6f622c365283e0796e7
Fredrik Löfgren <fredrik@eaproduktion.se>
2018-04-06 11:02:16 +0000

    Add chat client / server

--- server.js
+++ server.js
@@ -1,10 +1,26 @@
-var express = require('express')
-var app = express()
+var app = require('express')()
+var http = require('http').Server(app)
+var io = require('socket.io')(http)
 
-app.get('/', function (req, res) {
-    res.send('Hello Digital Ocean!')
+app.get('/', function(req, res){
+  res.sendFile(__dirname + '/views/index.html');
 })
 
-app.listen(3000, function () {
-    console.log('Magic is happening on port 3000!')
+io.on('connection', function(socket){
+  console.log('a user connected')
+  socket.on('disconnect', function(){
+    console.log('user disconnected')
+  })
+  socket.on('chat message', function(msg){
+    console.log('message: ' + msg);
+    io.emit('chat message', msg);
+  });
+})
+
+
+
+
+
+http.listen(3000, function () {
+  console.log('Magic is happening on port 3000!')
 })
