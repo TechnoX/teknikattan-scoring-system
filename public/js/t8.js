8f2ea09005d4a35f7f2981162139c4418e1d84a3
Fredrik Löfgren <fredrik@eaproduktion.se>
2018-05-03 14:42:09 +0000

    Change competitior and projector to dirctives
    Load all slides to the timeline

--- public/js/t8.js
+++ public/js/t8.js
@@ -2,7 +2,29 @@
 
 var app = angular.module('t8', ['ui.tinymce']);
 app.controller('editorCtrl', function($scope) {
+    $scope.slides = [
+        {index: 1, title: "TitelPåFråga", image: "/images/ogonmatt.jpg", timeText: "15 sekunder per påstående", scoringText: "1 poäng per rätt påstående", maxScoringText: "Totalt 6 poäng", time: 15, hasTimer: true, textLeft: "<p>Initial <strong>content</strong> left</p>", textRight: "<p style='color: red;'>Initial <strong>content</strong> right</p>", textProjector: "Initial <strong>content</strong> projector"},
+        {index: 2, title: "Solförmörkelser", image: "/images/solformorkelse.jpg", timeText: "4 minuter", scoringText: "2 poäng per rätt", maxScoringText: "Totalt 6 poäng", time: 4*60, hasTimer: true, textLeft: "<p>Lite mer text.. ASft. eft</p>", textRight: "<p style='color: red;'>Initial <strong>content</strong> right</p>", textProjector: "Initial <strong>content</strong> projector"},
+        {index: 3, title: "Spagettitorn", image: "/images/spagettitorn.jpg", timeText: "2 minuer", scoringText: "5 poäng per rätt påstående", maxScoringText: "10 poäng", time: 2*60, hasTimer: true, textLeft: "<p>Initial adsas aasd a sd asd left</p>", textRight: "<p>Initial <strong>content</strong> right</p>", textProjector: "Initial <strong>content</strong> projector"},
+        {index: 4, title: "Arbetsfördelning", image: "/images/arbetsfordelning.jpg", timeText: "15 sekunder per påstående", scoringText: "1 poäng per rätt påstående", maxScoringText: "Totalt 6 poäng", time: 15, hasTimer: true, textLeft: "<p>Initial <strong>content</strong> left</p>", textRight: "<p style='color: red;'>Initial <strong>content</strong> right</p>", textProjector: "Initial <strong>content</strong> projector"},
+        {index: 5, title: "Lampor", image: "/images/lampor.jpg", timeText: "15 sekunder per påstående", scoringText: "1 poäng per rätt påstående", maxScoringText: "Totalt 6 poäng", time: 15, hasTimer: true, textLeft: "<p>Initial <strong>content</strong> left</p>", textRight: "<p style='color: red;'>Initial <strong>content</strong> right</p>", textProjector: "Initial <strong>content</strong> projector"}];
 
+
+    $scope.currIndex = 0;
+    $scope.numHints = 1;
+    $scope.numTruefalse = 1;
+    $scope.numQuestions = 1;
+    $scope.numAlternatives = 1;
+    $scope.numPairsB = 1;
+    $scope.numPairsA = 1;
+    /*
+    $scope.addNewSlider = function () {
+        $scope.slides.push({
+            title: 'NyFråga',
+            image: '/asd/asd'
+        });
+    };*/
+    
     $scope.tinymceOptions = {
         theme: 'inlite',
         plugins: 'image media table link paste contextmenu textpattern autolink',
@@ -14,21 +36,6 @@ app.controller('editorCtrl', function($scope) {
         content_css: []
     };
     
-    $scope.numHints = 1;
-    $scope.numTruefalse = 1;
-    $scope.numQuestions = 1;
-    $scope.numAlternatives = 1;
-    $scope.numPairsB = 1;
-    $scope.numPairsA = 1;
-    $scope.title = "1. TitelPåFråga";
-    $scope.timeText = "15 sekunder per påstående";
-    $scope.scoringText = "1 poäng per rätt påstående";
-    $scope.maxScoringText = "Totalt 6 poäng";
-    $scope.time = 15;
-    $scope.textLeft = "<p>Initial <strong>content</strong> left</p>";
-    $scope.textRight = "<p style='color: red;'>Initial <strong>content</strong> right</p>";
-    $scope.textProjector = "Initial <strong>content</strong> projector";
-
     
     $scope.range = function(n) {
         return new Array(n);
@@ -36,8 +43,31 @@ app.controller('editorCtrl', function($scope) {
 
 });
 
-app.controller('questionCtrl', ['$scope', '$sce', function($scope, $sce) {
-    $scope.numHints = 1;
+
+
+app.directive("competitor", function() {
+    return {
+        templateUrl: "/template/competitor.html",
+        scope: {
+            slide: '@',
+        },
+        restrict: "E"
+    }
+});
+
+
+app.directive("projector", function(){
+    return {
+        templateUrl: "/template/projector.html",
+        restrict: "E"
+    }
+});
+
+
+
+
+app.controller('questionCtrl', function(){
+/*    $scope.numHints = 1;
     $scope.numTruefalse = 1;
     $scope.numQuestions = 1;
     $scope.numAlternatives = 1;
@@ -52,26 +82,9 @@ app.controller('questionCtrl', ['$scope', '$sce', function($scope, $sce) {
     $scope.textLeft =  $sce.trustAsHtml("<p>Initial <strong>content</strong> left</p>");
     $scope.textRight =  $sce.trustAsHtml("<p style='color: red;'>Initial <strong>content</strong> right</p>");
     $scope.textProjector =  $sce.trustAsHtml("Initial <strong>content</strong> projector");
-
-    
-}]);
-
-app.directive("competitor", function(){
-    return {
-        templateUrl: "/template/competitor.html",
-        restrict: "E",
-        link: function(scope, el, attr) {
-            scope.some_value = scope.some_value;
-            scope.updateChg = function(){
-                alert(scope.some_value)
-            }
-        }
-    }
+*/
+    // TODO: Implement
 });
-
-
-
-
 
 
 app.controller('answerCtrl', function($scope) {
