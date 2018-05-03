6b65265ce876e4944ece2aff3aa3b4c496f3c4c3
Fredrik Löfgren <fredrik@eaproduktion.se>
2018-05-03 10:19:48 +0000

    Add tinyMCE editor for body text

--- public/js/t8.js
+++ public/js/t8.js
@@ -1,21 +1,19 @@
 
 
-var app = angular.module('t8', []);
+var app = angular.module('t8', ['ui.tinymce']);
 app.controller('editorCtrl', function($scope) {
 
-    $scope.$on('$includeContentLoaded', function(event) {
-        tinymce.init({
-            selector: 'div.tinymce',
-            theme: 'inlite',
-            plugins: 'image media table link paste contextmenu textpattern autolink codesample',
-            insert_toolbar: 'quickimage quicktable media codesample',
-            selection_toolbar: 'bold italic | quicklink h2 h3',
-            inline: true,
-            paste_data_images: true,
-            content_css: []
-        });
-    });
-
+    $scope.tinymceOptions = {
+        theme: 'inlite',
+        plugins: 'image media table link paste contextmenu textpattern autolink',
+        insert_toolbar: 'quickimage quicktable media',
+        selection_toolbar: 'bold italic | quicklink h2 h3',
+        inline: true,
+        forced_root_block : false,
+        paste_data_images: true,
+        content_css: []
+    };
+    
     $scope.numHints = 1;
     $scope.numTruefalse = 1;
     $scope.numQuestions = 1;
@@ -26,7 +24,10 @@ app.controller('editorCtrl', function($scope) {
     $scope.timeText = "15 sekunder per påstående";
     $scope.scoringText = "1 poäng per rätt påstående";
     $scope.maxScoringText = "Totalt 6 poäng";
-    $scope.time = "00:15";
+    $scope.time = 15;
+    $scope.textLeft = "<p>Initial <strong>content</strong> left</p>";
+    $scope.textRight = "<p style='color: red;'>Initial <strong>content</strong> right</p>";
+    $scope.textProjector = "Initial <strong>content</strong> projector";
 
     
     $scope.range = function(n) {
