1024cd753af1b93232b74848e48ff0e551c38391
Fredrik Löfgren <fredrik@eaproduktion.se>
2018-05-13 15:10:57 +0000

    Set focus to active hint

--- public/js/t8.js
+++ public/js/t8.js
@@ -379,3 +379,18 @@ app.directive('stringToNumber', function() {
         }
     };
 });
+app.directive('setFocus', function($timeout, $parse) {
+    return {
+        //scope: true,   // optionally create a child scope
+        link: function(scope, element, attrs) {
+            var model = $parse(attrs.setFocus);
+            scope.$watch(model, function(value) {
+                if(value === true) {
+                    $timeout(function() {
+                        element[0].focus();
+                    });
+                }
+            });
+        }
+    };
+});
