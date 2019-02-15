d4235aab249692fe413326bd72f2366d44e3dd89
Fredrik Löfgren <fredrik@eaproduktion.se>
2019-02-15 22:28:24 +0000

    Add competition view

--- public/js/t8.js
+++ public/js/t8.js
@@ -576,7 +576,14 @@ app.controller('audienceCtrl', ['$scope', '$http', function($scope, $http){
 
 
 app.controller('mainCtrl', ['$scope', '$http', function($scope, $http){
-    $scope.cities = [{name: "Sverige", id: 0}, {name: "Linköping", id: 1}, {name: "Uppsala", id: 2}, {name: "Borlänge", id: 3}, {name: "Stockholm", id: 4} ];
+    $scope.competitions = [
+        {id: 23, name: "Regionsemifinal 1", city: 0, lastEdited: new Date(2018,3,24,10,22), teams:['Skolgårda skola', 'Berzeliusskolan', 'Södervärnsskolan']},
+        {id: 36, name: "Regionsemifinal 2", city: 0, lastEdited: new Date(2018,3,24,10,28), teams:['Sjöängsskolan', 'Malmlättsskolan', 'Hultdalskolan']},
+        {id: 74, name: "Regionfinal", city: 1, lastEdited: new Date(2018,3,24,11,22), teams:['Sjöängsskolan', 'Skolgårda skola', 'Berzeliusskolan']},
+        {id: 92, name: "Riksfinal", city: 1, lastEdited: new Date(2018,4,22,08,22), teams:['IFM', 'MAI', 'IDA']}
+    ];
+    
+    $scope.cities = [{name: "Sverige", id: 0}, {name: "Linköping", id: 1}, {name: "Uppsala", id: 2}, {name: "Borlänge", id: 3}, {name: "Stockholm", id: 4}, {name: "Lund", id: 7} ];
     $scope.users = [{id: 0, name: "Fredrik", city: 0, password: "giraff"}, {id: 3, name: "Peter", city: 0, password: "retep"}, {id: 2, name: "Susanne", city: 1, password: "ennasus"}, {id: 7, name: "Röde", city: 3, password: "lego"}];
     
     $scope.editPassword = function(user){
@@ -605,6 +612,7 @@ app.controller('mainCtrl', ['$scope', '$http', function($scope, $http){
         }
         $scope.users.push({name: $scope.newName, city: $scope.newCity, password: password, id: max.id+1});
         $scope.newName = "";
+        $scope.newCity = 0;
     }
     
     $scope.removeCity = function(city){
@@ -631,6 +639,34 @@ app.controller('mainCtrl', ['$scope', '$http', function($scope, $http){
         $scope.cities.push({name: $scope.newCity, id: max.id+1});
         $scope.newCity = "";
     }
+
+    $scope.editCompetition = function(comp){
+        console.log(comp);
+        comp.lastEdited = new Date();
+    }
+    
+    $scope.removeCompetition = function(comp){
+        if(confirm("Vill du verkligen ta bort " + comp.name)){
+            $scope.competitions = $scope.competitions.filter(x => x.id !== comp.id);
+        }
+    }
+    
+    $scope.cloneCompetition = function(comp){
+        var newComp;
+        newComp = angular.copy(comp);
+        newComp.lastEdited = new Date();
+        newComp.id = 1 + Math.max.apply(Math, $scope.competitions.map(function(o) { return o.id; }));
+        $scope.competitions.push(newComp);
+    }
+    
+    $scope.addCompetition = function(){
+        var max = Math.max.apply(Math, $scope.competitions.map(function(o) { return o.id; }));
+        console.log(max)
+        $scope.competitions.push({name: $scope.newName, city: $scope.newCity, lastEdited: new Date(), teams: [], id: max+1});
+        $scope.newName = "";
+        $scope.newCity = 0;
+    }
+
     
 }]);
 
