573aa7c888ddfe54827b558612f3964e27a9665a
Fredrik Löfgren <fredrik@eaproduktion.se>
2019-02-27 19:53:14 +0000

    Add timer for the backend when pressing next and previous it controls the timer start / stop as well as page changes

--- socket.js
+++ socket.js
@@ -2,6 +2,53 @@ var db = require('./db');
 //var fsm = require('./fsm');
 var io_;
 
+var timers = {};
+
+class Timer {
+    constructor(competition_id, duration) {
+        console.log("Inside constructor");
+        console.log(competition_id, duration);
+        this.competition_id = competition_id;
+        this.duration = duration;
+        this.end = new Date();
+        this.end.setSeconds(this.end.getSeconds() + duration);
+        this.started = false;
+    }
+
+    timeTick(){
+        var msg = {};
+        msg.competition = this.competition_id;
+        msg.time = Math.round((this.end - new Date()) / 1000);
+        io_.emit('time', msg);
+        if(msg.time <= 0){
+            this.stop();
+            io_.emit('timesUp', msg);
+        }
+    }
+    start() {
+        var o = this;
+        this.timer = setInterval(function(){o.timeTick();}, 1000);
+        this.started = true;
+    }
+    setDuration(duration) {
+        this.duration = duration;
+        this.reset()
+    }
+    stop() {
+        clearInterval(this.timer);        
+        this.started = false;
+    }
+    reset() {
+        this.end = new Date();
+        this.end.setSeconds(this.end.getSeconds() + this.duration);
+        var msg = {};
+        msg.competition = this.competition_id;
+        msg.time = Math.round((this.end - new Date()) / 1000);
+        io_.emit('time', msg);
+    }
+}
+
+
 exports.interface = function(io) {
 
     io_ = io;
@@ -15,28 +62,96 @@ exports.interface = function(io) {
         });
         socket.on('next', function(msg){
             console.log("Next pressed!");
-            db.increase_index(msg.competition, function(err){
-                if(err){
-                    console.error(err);
-                }else{
-                    socket.emit('view_changed', msg.competition);
-                }
-            });
+            nextPressed(msg.competition);
+
         });
         socket.on('prev', function(msg){
             console.log("Previous pressed!");
-            db.decrease_index(msg.competition, function(err){
-                if(err){
-                    console.error(err);
-                }else{
-                    socket.emit('view_changed', msg.competition);
-                }
-            });
+            previousPressed(msg.competition);
         });
 
     });
 
 };
+
+function previousPressed(competition_id){
+    db.get_slide(competition_id, 0, function(err, slide){
+        if (err){
+            console.error(err);
+        }else{
+            if(slide.hasTimer && timers[competition_id] && timers[competition_id].started){
+                console.log("Had timer started, reset it");
+                timers[competition_id].stop();
+                timers[competition_id].reset();
+            }else{
+                console.log("Goto previous slide");
+                gotoPrevSlide(competition_id);
+            }
+        }
+    });
+}
+
+function nextPressed(competition_id){
+    db.get_slide(competition_id, 0, function(err, slide){
+        if (err){
+            console.error(err);
+        }else{
+            if(slide.hasTimer && (!timers[competition_id] || !timers[competition_id].started)){
+                console.log("Slide has timer");
+                if(!timers[competition_id]){
+                    console.log("Outside");
+                    console.log(competition_id, slide.time);
+                    timers[competition_id] = new Timer(competition_id, slide.time);
+                    console.log("Create new timer");
+                }else{
+                    timers[competition_id].setDuration(slide.time);
+                    console.log("Update new duration");
+                }
+                timers[competition_id].start();
+                console.log("Start timer");
+            }else{
+                console.log("Goto new slide");
+                gotoNextSlide(competition_id);
+            }
+        }
+    });
+}
+
+
+
+function gotoNextSlide(competition_id){
+    db.increase_index(competition_id, function(err){
+        if(err){
+            console.error(err);
+        }else{
+            db.get_slide(competition_id, 0, function(err, slide){
+                if (err){
+                    console.error(err);
+                }else{
+                    io_.emit('view_changed', competition_id);//slide);
+                }
+            });
+        }
+    });
+}
+
+function gotoPrevSlide(competition_id){
+    db.decrease_index(competition_id, function(err){
+        if(err){
+            console.error(err);
+        }else{
+            db.get_slide(competition_id, 0, function(err, slide){
+                if (err){
+                    console.error(err);
+                }else{
+                    io_.emit('view_changed', competition_id);//slide);
+                }
+            });
+        }
+    });
+}
+
+
 
 exports.change_state = function(state) {
     io_.emit('stateChange', state);
