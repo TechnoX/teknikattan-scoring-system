7fb6422bd2fecd385c3ca5b60b2c284226b3ed70
Fredrik Löfgren <fredrik@eaproduktion.se>
2019-03-05 12:38:31 +0000

    Remove refresh of side on each change of view, since it was slow and laggy.
    This will also remove the need o saving between each new move snce the questions array will not get updated at every move

--- public/js/neweditorCtrl.js
+++ public/js/neweditorCtrl.js
@@ -1,14 +1,7 @@
 app.controller('neweditorCtrl', ['$scope', '$http', '$routeParams', function ($scope, $http, $routeParams) {
     $scope.competitionId = parseInt($routeParams.id);
-    if($routeParams.slide && $routeParams.question){
-        $scope.slideIndex = parseInt($routeParams.slide);
-        $scope.questionIndex = parseInt($routeParams.question);
-    }else{
-        $scope.slideIndex = 0;
-        $scope.questionIndex = 0;
-    }
-    console.log("Question: "+ $scope.questionIndex + ", Slide: " + $scope.slideIndex);
-    
+    $scope.slideIndex = 0;
+    $scope.questionIndex = 0;
     switch($routeParams.medium){
     case 'projector':
         $scope.medium = "projector";
@@ -26,14 +19,24 @@ app.controller('neweditorCtrl', ['$scope', '$http', '$routeParams', function ($s
     $scope.nextDown = {'projector': 'competitor', 'competitor': 'answer'};
     $scope.nextUp = {'answer': 'competitor', 'competitor': 'projector'};
     
+    // Get all questions
     $http.get('/competition/'+$scope.competitionId+'/questions').then(function(resp) {
         $scope.questions = resp.data;
         if($scope.questions.length > 0){
-            $scope.currQuestion = $scope.questions[$scope.questionIndex];
-            $scope.currSlide = $scope.currQuestion.slides[$scope.slideIndex];
+            $scope.currQuestion = $scope.questions[0];
+            $scope.currSlide = $scope.currQuestion.slides[0];
         }
     });
     
+    
+    
+    $scope.changeSlide = function(questionIndex, slideIndex, medium){
+        $scope.questionIndex = questionIndex;
+        $scope.slideIndex = slideIndex;
+        $scope.medium = medium;
+        $scope.currQuestion = $scope.questions[$scope.questionIndex];
+        $scope.currSlide = $scope.currQuestion.slides[$scope.slideIndex];
+    }
     
     $scope.editOptions = {
         theme: 'inlite',
