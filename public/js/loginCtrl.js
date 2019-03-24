8744c54ff2600e7b34dff5261f5dad3658a61b3e
Fredrik Löfgren <fredrik@eaproduktion.se>
2019-03-24 20:04:45 +0000

    Send username and password to backend, forgot the dot rule in ng-model

--- public/js/loginCtrl.js
+++ public/js/loginCtrl.js
@@ -1,14 +1,22 @@
 app.controller('loginCtrl', ['$scope', '$http', function($scope, $http){
+
+    $scope.data = {};
+    $scope.data.username = "";
+    $scope.data.password = "";
+    
+    
     $scope.logout = function(){
         localStorage.removeItem('t8_token');
         $scope.isAuthenticated = false;
         alert("Du har loggats ut!");
     }
-        
+ 
     $scope.login = function(){
-        $http.post("/login", {username: $scope.username, password: $scope.password}).then(function(res) {
+        console.log("Send data: ", $scope.data);
+        $http.post("/login", $scope.data).then(function(res) {
             if(res.data.success){
                 localStorage.setItem("t8_token", res.data.token);
+                $scope.isAuthenticated = true;
                 console.log("response", res.data);
             }
         }, function(res){
