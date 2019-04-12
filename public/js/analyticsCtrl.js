091be96b9313222d65852196d60cbfd26b6d874b
Fredrik Löfgren <fredrik@eaproduktion.se>
2019-04-12 15:18:20 +0000

    Add analytics view to see how teams answered the questions

--- public/js/analyticsCtrl.js
+++ public/js/analyticsCtrl.js
@@ -10,6 +10,20 @@ app.controller('analyticsCtrl', ['$scope', '$http', '$routeParams', function($sc
         }
     });
     
+    $http.get('/competition/'+competition_id+'/questions').then(function(resp) {
+        $scope.questions = resp.data;
+        for(var i = 0; i < $scope.questions.length; i++){
+            if($scope.questions[i].type == "normal"){
+                $scope.questions[i].state = "question";
+            }else if($scope.questions[i].type == "truefalse"){
+                $scope.questions[i].state = "statements";
+            }else{
+                $scope.questions[i].state = $scope.questions[i].type;
+            }
+        }
+    });
+
+    
     $scope.getTotalScore = function(team){
         var total = 0;
         for(var t = 0; t < team.scores.length; t++){
