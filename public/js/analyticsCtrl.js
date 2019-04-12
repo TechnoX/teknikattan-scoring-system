0a4a63d913415a4a00a4b5b06e0c464e2dfe0774
Fredrik Löfgren <fredrik@eaproduktion.se>
2019-04-12 19:00:50 +0000

    Loop through all IDs and get teams / competitions

--- public/js/analyticsCtrl.js
+++ public/js/analyticsCtrl.js
@@ -1,28 +1,34 @@
 app.controller('analyticsCtrl', ['$scope', '$http', '$routeParams', function($scope, $http, $routeParams){
     var socket = io();
-    var competition_id = $routeParams.id;
-    $scope.IDs = $routeParams.ids;
-    
+    var competition_ids = $routeParams.ids;
+    if(!Array.isArray(competition_ids)){
+        competition_ids = [competition_ids];
+    }
+
     $scope.teams = [];
-    $http.get('/api/competition/'+competition_id+'/teams').then(function(resp) {
-        if(resp.data){
-            $scope.teams = resp.data;
-            console.log("Set teams to ", $scope.teams);
-        }
-    });
     
-    $http.get('/api/competition/'+competition_id+'/questions').then(function(resp) {
-        $scope.questions = resp.data;
-        for(var i = 0; i < $scope.questions.length; i++){
-            if($scope.questions[i].type == "normal"){
-                $scope.questions[i].state = "question";
-            }else if($scope.questions[i].type == "truefalse"){
-                $scope.questions[i].state = "statements";
-            }else{
-                $scope.questions[i].state = $scope.questions[i].type;
+    for(var i = 0; i < competition_ids.length; i++){
+        
+        $http.get('/api/competition/'+competition_ids[i]+'/teams').then(function(resp) {
+            if(resp.data){
+                $scope.teams.push(...resp.data);
+                console.log("Set teams to ", $scope.teams);
             }
-        }
-    });
+        });
+        
+        $http.get('/api/competition/'+competition_ids[i]+'/questions').then(function(resp) {
+            $scope.questions = resp.data;
+            for(var i = 0; i < $scope.questions.length; i++){
+                if($scope.questions[i].type == "normal"){
+                    $scope.questions[i].state = "question";
+                }else if($scope.questions[i].type == "truefalse"){
+                    $scope.questions[i].state = "statements";
+                }else{
+                    $scope.questions[i].state = $scope.questions[i].type;
+                }
+            }
+        });
+    }
 
     
     $scope.getTotalScore = function(team){
