c70ee12d68229d47b1f8fe6f643e4aa1863f0473
Fredrik Löfgren <frelo223@student.liu.se>
2019-12-09 14:33:27 +0100

    Add alert box when no name is set on new competition

--- public/js/managementCtrl.js
+++ public/js/managementCtrl.js
@@ -166,6 +166,11 @@ app.controller('managementCtrl', ['$scope', '$http', '$routeParams', '$uibModal'
     }
     
     $scope.addCompetition = function(){
+        if (!$scope.newName){
+            alert("Du måste ange ett namn på tävlingen.");
+            return;
+        }
+        
         var newComp = {name: $scope.newName, city: $scope.newCity, lastEdited: (new Date()).toISOString(), index: 0};
         addCompetition(newComp, undefined, function(err, id){
             if(err)return;
